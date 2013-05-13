a3f94b67465a28cba770b86e3723af20cc40c2fe
reznorsedge <github@sjdowling.net>
2013-05-13 09:17:13 +0100

    WIP: Now actually works on the Pi

--- time_lapse.py
+++ time_lapse.py
@@ -11,7 +11,7 @@ class Camera:
         self.log = logging.getLogger('time_lapse.Camera')
 
     def take_photo(self, iteration, output_dir):
-        self.log.debug('Take a picture. Iteration "%s"' % (iteration))
+        self.log.info('Take a picture. Iteration "%s"' % (iteration))
         output_file = output_dir + "/" + str(iteration).zfill(4) + ".jpg"
         subprocess.call(["fswebcam", "--save", output_file, "-r", "1280x720",
                 "-q", "-d", "v4l2:/dev/video0", "-S", "15"])
@@ -35,22 +35,27 @@ class Camera:
         Create a movie using a list of files inside a file.
         '''
         output_file = os.path.join(output_dir, output_name)
+        input_path = os.path.join(output_dir, input_file)
         self.log.info(
-                'Create a video from list of filenames (%s)' % (input_file))
+                'Create a video from list of filenames (%s)' % (input_path))
         self.log.debug('Running command with output dir "%s" and output file'
                         '%s' % (output_dir, output_name))
-        subprocess.call(["mencoder", "mf://@%s" % input_file, "-mf",
+        subprocess.call(["mencoder", "mf://@%s" % input_path, "-mf",
                  "fps=24:type=jpg", "-ovc", "lavc", "-lavcopts",
                  "vcodec=mpeg4:mbd=2:trell", "-oac", "copy", "-o",
                  output_file])
 
     def analyse_files(self, images_location, too_dark='too_dark.txt',
-                      ok_images='ok_images.txt', percent_black=98.5):
+                      ok_images='ok_images.txt', percent_black=96.0):
         '''
         Create two lists of files. One has the files which are deemed useful
         to the video, the other is ones to discard (i.e. too dark to be used)
         '''
         list_of_files = []
+        too_dark_images_list = []
+        ok_images_list = []
+        too_dark_filename = os.path.join(images_location, too_dark)
+        ok_files_filename = os.path.join(images_location, ok_images)
 
         self.log.debug('Build up a list of files to check')
         for files in os.listdir(images_location):
@@ -58,20 +63,35 @@ class Camera:
                 list_of_files.append(files)
 
         self.log.debug('Look at the histogram and put in the correct file')
-        too_dark_file = open(too_dark, 'w')
-        ok_files_file = open(ok_images, 'w')
+
+        too_dark_file = open(too_dark_filename, 'w')
+        ok_files_file = open(ok_files_filename, 'w')
         for target_file in list_of_files:
-            image_file = Image.open(target_file)
+            image_file_name = os.path.join(images_location, target_file)
+            image_file = Image.open(image_file_name)
             image_file = image_file.convert('1')
             hist = image_file.histogram()
-            if (100 * (float(hist[0] / float(sum(hist))))) > percent_black:
-                too_dark_file.write('%s\n' % target_file)
+            if (100 * (float(hist[0]) / float(sum(hist)))) > float(percent_black):
+                too_dark_images_list.append(target_file)
             else:
-                ok_files_file.write('%s\n' % target_file)
+                ok_images_list.append(target_file)
+
+        too_dark_images_list.sort()
+        ok_images_list.sort()
+        for target_image in too_dark_images_list:
+            too_dark_file.write('%s\n' % target_image)
+        for target_image in ok_images_list:    
+            ok_files_file.write('%s\n' % target_image)
 
         self.log.debug('Close the files')
         too_dark_file.close()
         ok_files_file.close()
+
+        self.log.info('toodark: %s\n\nok: %s\n\n' % (too_dark_images_list, ok_images_list))
+        self.log.info('Total images: %s\n'
+                      'Light images: %s\n'
+                      'Dark images : %s\n' % (len(list_of_files), 
+                      len(ok_images_list), len(too_dark_images_list)))
         return too_dark, ok_images
 
 
@@ -104,7 +124,7 @@ def main():
     log.info('Top of Main')
 
     # 24 hours = 1440 mins
-    time_to_run_mins = 1
+    time_to_run_mins = 2
     delay_between_shots = 10
 
     timenow = time.time()
@@ -119,20 +139,20 @@ def main():
         iteration += 1
         time.sleep(delay_between_shots)
 
-    log.info('Create a film with all images')
-    photophoto.create_film_from_files(output_dir)
-
     log.info('Generate lists of light and dark images')
-    too_dark, ok_images = photophoto.analyse_files(output_dir, 
-                                                   'time_lapse_all.avi')
+    too_dark_2, ok_images = photophoto.analyse_files(output_dir)
 
     log.info('Create a film with only dark images')
-    photophoto.create_film_from_list(too_dark, output_dir,
+    photophoto.create_film_from_list(too_dark_2, output_dir,
                                      'time_lapse_dark.avi')
 
     log.info('Create a film without dark images')
     photophoto.create_film_from_list(ok_images, output_dir,
                                      'time_lapse_light.avi')
+
+    log.info('Create a film with all images')
+    photophoto.create_film_from_files(output_dir, 'time_lapse_all.avi')
+
     log.info('And we are DONE!')
 
 
